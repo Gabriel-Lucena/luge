fc63a07601baaecb2807370e388a4b7b5e70b1d9
Gabriel-Lucena <gabriel.lucena.cunha@hotmail.com>
2022-04-02 21:10:02 -0300

    Teste do heroku

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const routes = express.Router();
 
+const UserController = require("./controllers/UserController");
+
 routes.get("/", (request, response) => {
   response.send("Olá");
 });
+
+routes.get("/user", UserController.index);
+
 
 module.exports = routes;
